199c6e94b5147e759a632966bf4124347cb3c622
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-11-15 11:28:48 +0100

    experiece page

---
+++ app/experience/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='text-white flex flex-col items-center px-20 py-10'>
+        <h1>./experience</h1>
+
+        <div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default page
